b2e58a2b3e30c0cfbf1dc08be4f85aa55ccf4fb6
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2022-08-31 22:28:55 +0530

    Add files via upload
    first commit

---
+++ index.pack.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+    Task: Comparing the search query vs. the DOM names
+    1. Create a conditional that checks if currentName is equal to the user's search query!
+    2. If our conditional returns true, set the display style on 'allNamesDOMCollection[counter]' to be a block element
+    3. If our conditional falls into the else, we have no match! So set the display style to 'none'
+    
+    Helpers: use the methods 'includes' for objective 1
+    - The includes() method determines whether one string may be found within another string, returning true or false as appropriate.
+*/
+document.getElementById("searchInput").addEventListener("keyup", function (event) {
+    var searchQuery = event.target.value.toLowerCase();
+    var allNamesDOMCollection = document.getElementsByClassName('name');
+
+    for (var counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        var currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
+    }
+});
+
+/***/ })
+/******/ ]);
